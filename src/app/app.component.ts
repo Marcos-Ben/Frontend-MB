45a830e5aae3a4e9093b262b88c5fd0138c205ce
Marcos-Ben <marcosbenencia98@gmail.com>
2024-03-11 08:43:59 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './Componentes/header/header.component';
+import { SobreMiComponent } from './Componentes/sobre-mi/sobre-mi.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, SobreMiComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
